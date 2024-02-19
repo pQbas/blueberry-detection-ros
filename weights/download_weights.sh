6ca0ee709cca84f1fc10d0342f82a4c57e7f7389
pQbas <percybrayan18@gmail.com>
2024-02-19 17:13:04 -0500

    readme updated

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,2 +1,4 @@
 gdown https://drive.google.com/uc?id=1ZstfTBYxA4DCSRQaQDhdJA9MUgmSiT9j
+mv yolov8m_best.pt weights/
 gdown https://drive.google.com/uc?id=1IQglmgW0gp49k5CUlATHOgDEiD2mehX2
+mv best.pt weights/
